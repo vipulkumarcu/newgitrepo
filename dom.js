0c1e737b8ed970cdcb7872e0ac3d34c24b0a02e6
vipulkumarcu <95040221+vipulkumarcu@users.noreply.github.com>
2023-05-02 16:31:48 +0530

    Adding JavaScript File

---
+++ dom.js
@@ -0,0 +1,19 @@
+// console.dir(document);
+// console.log (document.domain); 
+// console.log (document.URL);
+// console.log (document.title);
+document.title = "Index Page";
+// console.log (document.forms);
+
+var headerTitle = document.getElementById ("header-title");
+
+var header = document.getElementById ("main-header");
+
+// headerTitle.style.borderBottom = "solid 5px black";
+
+header.style.borderBottom = "solid 3px black";
+
+var head2 = document.getElementById ("h2-head");
+
+head2.style.color = "green";
+head2.style.fontWeight = "bold";
